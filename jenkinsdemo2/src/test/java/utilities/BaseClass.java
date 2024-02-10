f2774f80c72c64931fb227f567e88154fd0ff8ec
akshara2608 <akshara.k2608@gmail.com>
2024-02-10 08:38:05 +0530

    second commit

--- jenkinsdemo2/src/test/java/utilities/BaseClass.java
+++ jenkinsdemo2/src/test/java/utilities/BaseClass.java
@@ -2,6 +2,7 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
@@ -9,15 +10,13 @@ import org.testng.annotations.Parameters;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-WebDriver driver;
+static WebDriver driver;
 	@Parameters({"browser","url"})
 	@BeforeMethod
-	public void beforemethod()
+	public void beforemethod(String browser, String url)
 	{
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
-		driver.get("https://www.google.com/");
-		driver.manage().window().maximize();
+		
+		Ibrowser(browser,url);
 	}
 	
 	
@@ -26,8 +25,33 @@ WebDriver driver;
 	public void aftermethod()
 	{
 		driver.close();
+		System.out.println("browser closed");
 	}
 	
+	public static WebDriver Ibrowser(String NameOfBrowser, String url) {
+		if(NameOfBrowser.equalsIgnoreCase("chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			
+				driver=new ChromeDriver();
+			
+			
+		}
+		else if(NameOfBrowser.equalsIgnoreCase("edge"))
+		{
+			WebDriverManager.edgedriver().setup();
+			driver= new EdgeDriver();
+			
+		}
+			driver.manage().window().maximize();
+		
+			driver.manage().deleteAllCookies();
+			driver.get(url);
+		
+			return driver;
+	
+
+	}
 	
 	
 }
